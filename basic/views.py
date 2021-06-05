c0da3cc3a968504b171e59a6dc97e00de45f42b1
anton.zayniev <anton.zayniev@team.wrike.com>
2021-06-06 01:46:57 +0300

    add global site fixes
    * add a lot of site urls
    * make home login required
    * add navbar with user data

--- basic/views.py
+++ basic/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from matches.models import Match
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def home(request):
     matches = (Match.objects.filter(home_score__isnull=False) |
                Match.objects.filter(guest_score__isnull=False))
