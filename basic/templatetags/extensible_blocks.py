e999b0e28d167721d779144d24182680547eb297
ToxaZ <7903257+ToxaZ@users.noreply.github.com>
2021-06-13 23:27:38 +0300

    fix prediction calculations v2

--- basic/templatetags/extensible_blocks.py
+++ basic/templatetags/extensible_blocks.py
@@ -28,7 +28,7 @@ def cup_standings():
         results_data = get_user_results_by_matches(user.id, matches_queryset)
         for match_data in results_data.values():
             total_points += sum(list(filter(
-                None, [match_data['result_bet'], match_data['score_bet']])), 2)
+                None, [match_data['result_bet'], match_data['score_bet']])))
         standings.update({user: round(total_points, 2)})
     return {'results': standings}
 
