3023bbf642e91292c7eeae9674d9d39b2b8a64f9
ToxaZ <7903257+ToxaZ@users.noreply.github.com>
2021-06-14 01:31:42 +0300

    correct winner submissions closure

--- predictions/views.py
+++ predictions/views.py
@@ -12,6 +12,7 @@ from predictions.forms import NewPredictionForm, WinnerPredictionForm
 
 COMPETITION_START_DATE_UTC = datetime(2021, 5, 11, 19, 0, 0, tzinfo=pytz.utc)
 
+
 def available_coefficients(request):
     avail_coefs = last_prediction(
         Coefficient.objects
@@ -105,5 +106,5 @@ def winner_prediction(request):
     return render(request, 'winner.html',
                   {'winner_coef': winner_coef_data,
                    'curr_prediction': curr_prediction,
-                   'submissions_closed': datetime.now(pytz.UTC) < COMPETITION_START_DATE_UTC,
+                   'submissions_closed': datetime.now(pytz.UTC) > COMPETITION_START_DATE_UTC,
                    'form': frm})
