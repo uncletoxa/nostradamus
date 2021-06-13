5f2d9042bf2931306982cdd5c63062600dc2662d
ToxaZ <7903257+ToxaZ@users.noreply.github.com>
2021-06-13 20:36:49 +0300

    round digits in short standings table

--- basic/templatetags/extensible_blocks.py
+++ basic/templatetags/extensible_blocks.py
@@ -27,8 +27,8 @@ def cup_standings():
         total_points = 0
         results_data = get_user_results_by_matches(user.id, matches_queryset)
         for match_data in results_data.values():
-            total_points += sum(list(filter(
-                None, [match_data['result_bet'], match_data['score_bet']])))
+            total_points += round(sum(list(filter(
+                None, [match_data['result_bet'], match_data['score_bet']]))), 2)
         standings.update({user: total_points})
     return {'results': standings}
 
