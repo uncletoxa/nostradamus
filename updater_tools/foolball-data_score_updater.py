6f699d13f5d23cf9fb538ef431b7d60875fd7d51
ToxaZ <7903257+ToxaZ@users.noreply.github.com>
2021-06-15 00:51:57 +0300

    add link to admin and fix match statuses

--- updater_tools/foolball-data_score_updater.py
+++ updater_tools/foolball-data_score_updater.py
@@ -30,7 +30,7 @@ def update_scores(matches):
         if response.status == 200:
             resp = json.loads(response.read().decode())
             logging.info('Loading results: {}'.format(resp))
-            if resp['match']['status'] == 'IN_PLAY':
+            if resp['match']['status'] in ('IN_PLAY', 'PAUSED'):
                 match.is_live = True
             else:
                 match.is_live = False
