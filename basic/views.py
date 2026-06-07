96a431a8ca6980437a476ef7640bb146678d29af
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2026-06-07 14:51:19 +0200

    Stop redirecting users to winner prediction from home page
    Home now always renders home.html regardless of whether the user has
    made a winner prediction.

--- basic/views.py
+++ basic/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from predictions.models import WinnerPrediction
 
 
 @login_required
 def home(request):
-    curr_prediction = WinnerPrediction.objects.filter(user_id=request.user).first()
-    if curr_prediction:
-        return render(request, 'home.html')
-    else:
-        return HttpResponseRedirect("predictions/winner")
+    return render(request, 'home.html')
