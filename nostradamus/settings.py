b4ee631ac047552ff31e0102ee3bde40d72cf243
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2026-06-10 00:23:58 +0200

    Make password validators toggleable via DISABLE_PASSWORD_VALIDATORS env var

--- nostradamus/settings.py
+++ nostradamus/settings.py
@@ -77,19 +77,11 @@ WSGI_APPLICATION = 'nostradamus.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+AUTH_PASSWORD_VALIDATORS = [] if config('DISABLE_PASSWORD_VALIDATORS', default=False, cast=bool) else [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 
