2481df4cd9936ce3884b500505bcf67ac9f69cfd
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2026-06-10 16:49:49 +0200

    Register UserProfile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TeamSupporter, SupportedTeam
+from .models import TeamSupporter, SupportedTeam, UserProfile
 
 admin.site.register(TeamSupporter)
 admin.site.register(SupportedTeam)
+admin.site.register(UserProfile)
