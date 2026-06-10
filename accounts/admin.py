3e4def28ecd1fefa7e15c1e92c3a36037be2a5df
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2026-06-10 16:56:48 +0200

    Add UserProfile inline to User admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import TeamSupporter, SupportedTeam, UserProfile
 
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(TeamSupporter)
 admin.site.register(SupportedTeam)
-admin.site.register(UserProfile)
